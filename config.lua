b7e480458d6cbb7a0d616d2aa81372ed09e761d0
N0tZetsu <emamainn@gmail.com>
2025-04-16 01:38:30 -0300

    Add config.lua 1744778311502

---
+++ config.lua
@@ -0,0 +1,32 @@
+Config = {}
+
+-- Lista de canciones y sus videoclips correspondientes
+Config.MediaList = {
+    {
+        title = "Canción 1",
+        artist = "Artista 1",
+        music = "music/song1.mp3",
+        video = "video/video1.mp4",
+        cover = "https://images.genius.com/aeafd2c30ad7d3c7ef326f591983b5bc.1000x1000x1.png"
+    },
+    {
+        title = "Canción 2",
+        artist = "Artista 2",
+        music = "music/song2.mp3",
+        video = "video/video2.mp4",
+        cover = "https://images.genius.com/6b36c989cd85083238af56e55a7a0f80.1000x1000x1.jpg"
+    }
+}
+
+-- Configuración general
+Config.ServerName = "TU SERVIDOR"
+Config.Discord = "https://discord.gg/tuservidor"
+Config.Website = "https://tuservidor.com"
+
+-- Configuración de la barra de carga
+Config.LoadingBarColor = "#ff0000"
+Config.EnableLoadingBar = true
+
+-- Configuración de reproducción
+Config.AutoplayMusic = true
+Config.DefaultVolume = 0.5 -- 0.0 a 1.0
