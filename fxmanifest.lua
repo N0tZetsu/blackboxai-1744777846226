93106236c2a73b1f8f005ca3313abb9b88429a38
N0tZetsu <emamainn@gmail.com>
2025-04-16 01:30:49 -0300

    Add fxmanifest.lua 1744777850068

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'css/styles.css',
+    'js/script.js',
+    'assets/logo/*.png',
+    'assets/logo/*.jpg',
+    'assets/media/music/*.mp3',
+    'assets/media/video/*.mp4'
+}
